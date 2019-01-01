ab4ebb3a2ea1d8728accfe0559587a3dd1efe067
user148657 <user148657@ny009.local>
2019-01-01 18:23:35 -0500

    added Player model

---
+++ Swoosh App/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh App
+//
+//  Created by user148657 on 1/1/19.
+//  Copyright © 2019 user148657. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var selectedLeague: String?
+    var selectedSkillLevel: String? 
+}
